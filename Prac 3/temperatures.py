cb2a931832a595f7938577d8ab94bc65d5349d21
jc320748 <cain.mcdonald@my.jcu.edu.au>
2017-08-16 14:27:56 +1000

    Update prac 3

--- Prac 3/temperatures.py
+++ Prac 3/temperatures.py
@@ -2,18 +2,16 @@
 CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
-
+def main():
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
 while choice != "Q":
- if choice == "C":
-     celsius = float(input("Celsius: "))
-     fahrenheit = convert_celsius_to_fahrenheit(celsius)
-
-
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = convert_celsius_to_fahrenheit(celsius)
 
 
 def convert_celsius_to_fahrenheit(celsius):
@@ -22,18 +20,18 @@ def convert_celsius_to_fahrenheit(celsius):
 
 
 
-#     if choice == "C":
-#         celsius = float(input("Celsius: "))
-#         fahrenheit = celsius * 9.0 / 5 + 32
-#         print("Result: {:.2f} F".format(fahrenheit))
-#     elif choice == "F":
-#         # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-#         # Remove the "pass" statement when you are done. It's a placeholder.
-#         fahrenheit = float(input("Fahrenheit: "))
-#         celsius = (fahrenheit - 32) * 5 / 9
-#         print("Result: {:.2f} C".format(celsius))
-#     else:
-#         print("Invalid option")
-#     print(MENU)
-#     choice = input(">>> ").upper()
-# print("Thank you.")
+    #     if choice == "C":
+    #         celsius = float(input("Celsius: "))
+    #         fahrenheit = celsius * 9.0 / 5 + 32
+    #         print("Result: {:.2f} F".format(fahrenheit))
+    #     elif choice == "F":
+    #         # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+    #         # Remove the "pass" statement when you are done. It's a placeholder.
+    #         fahrenheit = float(input("Fahrenheit: "))
+    #         celsius = (fahrenheit - 32) * 5 / 9
+    #         print("Result: {:.2f} C".format(celsius))
+    #     else:
+    #         print("Invalid option")
+    #     print(MENU)
+    #     choice = input(">>> ").upper()
+    # print("Thank you.")
