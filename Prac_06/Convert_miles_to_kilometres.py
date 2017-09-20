d1c349797931329c2929f170bcf24f4187162357
jc320748 <cain.mcdonald@my.jcu.edu.au>
2017-09-20 11:07:22 +1000

    Finished Converting program

--- Prac_06/Convert_miles_to_kilometres.py
+++ Prac_06/Convert_miles_to_kilometres.py
@@ -7,4 +7,23 @@ class ConvertMilesToKilometres(App):
         self.title = 'Convert Miles To Kilometres'
         self.root = Builder.load_file('convert_miles_to_kilometres.kv')
         return self.root
+
+    def handle_convert(self):
+        value = self.validate_distance()
+        result = value * 1.60934
+        self.root.ids.output_label.text = str(result)
+
+    def validate_distance(self):
+        try:
+            value = float(self.root.ids.input_distance.text)
+            return value
+        except ValueError:
+            return 0
+
+    def handle_increment(self, change):
+        value = self.validate_distance() + change
+        self.root.ids.input_distance.text = str(value)
+        self.handle_convert()
+
+
 ConvertMilesToKilometres().run()
